3e684a62ba960c9b590d2b02ba322c0447d64897
Raman Raman Goyal <raman.goyal@sri.com>
2024-06-19 13:52:46 -0700

    with ORB detector

--- slam.py
+++ slam.py
@@ -1,13 +1,18 @@
+import cv2
 import numpy as np
-import scipy as sp
+import matplotlib.pyplot as plt
 
+# Create an ORB detector with a specified number of features
+orb = cv2.ORB_create(nfeatures=5000)
 
-a =10
-print(a*a)
+# Example usage with an image
+image = cv2.imread('RamanGoyal.jpg', cv2.IMREAD_GRAYSCALE)
+keypoints, descriptors = orb.detectAndCompute(image, None)
 
-b = 100
-print(b*b)
+# Draw keypoints on the image for visualization
+image_with_keypoints = cv2.drawKeypoints(image, keypoints, None, color=(0, 255, 0), flags=0)
 
-e = 1000
-print(e*e)
+plt.imshow(image_with_keypoints)
 
+# Save or display the image with keypoints
+cv2.imwrite('image_with_keypoints.jpg', image_with_keypoints)
